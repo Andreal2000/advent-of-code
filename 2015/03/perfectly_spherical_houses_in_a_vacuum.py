af5ef41db92020257bc4a34e907db2ac50c128d4
Andrea Longoni <andrealong2000@gmail.com>
2022-12-03 23:35:37 +0100

    Add 2015/03

---
+++ 2015/03/perfectly_spherical_houses_in_a_vacuum.py
@@ -0,0 +1,34 @@
+import os
+
+
+def part_one(input):
+    def add(a, b): return (a[0]+b[0], a[1]+b[1])
+    directions = {"^": (0, 1), "v": (0, -1), ">": (1, 0), "<": (-1, 0)}
+    position = (0, 0)
+    houses = [(0, 0)]
+    for i in input:
+        position = add(position, directions[i])
+        houses.append(position)
+    return len(set(houses))
+
+
+def part_two(input):
+    def add(a, b): return (a[0]+b[0], a[1]+b[1])
+    directions = {"^": (0, 1), "v": (0, -1), ">": (1, 0), "<": (-1, 0)}
+    santa = (0, 0)
+    robo = (0, 0)
+    houses = [(0, 0)]
+    for i in range(len(input)):
+        if i % 2 == 0:
+            santa = add(santa, directions[input[i]])
+            houses.append(santa)
+        else:
+            robo = add(robo, directions[input[i]])
+            houses.append(robo)
+    return len(set(houses))
+
+
+if __name__ == "__main__":
+    input = open(os.path.join(os.path.dirname(__file__), "input.txt")).read()
+    print(part_one(input))  # 2592
+    print(part_two(input))  # 2360
--- README.md
+++ README.md
@@ -6,3 +6,4 @@ My solutions to the [Advent of Code](https://adventofcode.com/) challenges
 
 01. [Not Quite Lisp](https://github.com/Andreal2000/advent-of-code/blob/main/2015/01)
 02. [I Was Told There Would Be No Math](https://github.com/Andreal2000/advent-of-code/blob/main/2015/02)
+03. [Perfectly Spherical Houses in a Vacuum](https://github.com/Andreal2000/advent-of-code/blob/main/2015/03)
